4c6624f4a4c2e2d7dc52bccf0374490f0e3f6c3b
luluagasantiago <luluagasantiago@icloud.com>
2024-03-19 12:46:44 +0100

    broken

--- go.mod
+++ go.mod
@@ -10,3 +10,5 @@ require (
 )
 
 require github.com/go-sql-driver/mysql v1.7.1
+
+require github.com/google/go-cmp v0.6.0 // indirect
---
+++ internal/auth/auth_test.go
@@ -0,0 +1,45 @@
+package auth
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestGetAPIKey(t *testing.T) {
+
+	tests := map[string]struct {
+		input      http.Header
+		wantString string
+		wantErr    error
+	}{
+		"No Header": {input: http.Header{}, wantString: "", wantErr: ErrNoAuthHeaderIncluded},
+		"Malforded": {input: http.Header{"Authorization": {"NotApiKey"}}, wantString: "", wantErr: MalformedAuthHeader},
+		"fail":      {input: http.Header{"Authorization": {"NotApiKey"}}, wantString: "", wantErr: "thisAINTWORK"},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			gotString, gotErr := GetAPIKey(tc.input)
+			diffString := cmp.Diff(tc.wantString, gotString)
+
+			// check the error first
+			// if we've got an error, Expeced out string doesn't matter
+			if !errors.Is(tc.wantErr, gotErr) {
+				fmt.Printf("%#v, %#v", tc.wantErr, gotErr)
+
+				t.Fatalf("%v != %v", tc.wantErr, gotErr)
+			}
+
+			if diffString != "" {
+				t.Fatalf(diffString)
+			}
+
+		})
+
+	}
+
+}
